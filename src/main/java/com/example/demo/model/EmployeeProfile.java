722b4ff7ee973f025e0a6dc7e6b6835b4abdecf4
bala-amypo <bala@amypo.in>
2025-12-17 05:32:58 +0000

    User commit

--- src/main/java/com/example/demo/model/EmployeeProfile.java
+++ src/main/java/com/example/demo/model/EmployeeProfile.java
@@ -2,13 +2,14 @@ package com.example.demo.model;
 
 import java.time.LocalDateTime;
 
-import org.springframework.boot.autoconfigure.web.WebProperties.Resources.Chain.Strategy;
+
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+@Entity
 public class EmployeeProfile {
     @Id
       @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,7 +17,7 @@ public class EmployeeProfile {
    @Column(unique = true)
     private String employeedId;
     private String fullName;
-    @Email
+   
    @Column(unique = true)
     private String email;
     private String teamName;
--- src/main/java/com/example/demo/model/LeaveRequest.java
+++ src/main/java/com/example/demo/model/LeaveRequest.java
@@ -1,13 +1,14 @@
 package com.example.demo.model;
 
-@Entity
+
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
-
+@Entity
 public class LeaveRequest {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
