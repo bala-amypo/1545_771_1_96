44c5ff4c2ea2e5b88e60db5adcd096fe356b2be1
bala-amypo <bala@amypo.in>
2025-12-17 05:03:06 +0000

    User commit

--- src/main/java/com/example/demo/model/EmployeeProfile.java
+++ src/main/java/com/example/demo/model/EmployeeProfile.java
@@ -2,25 +2,110 @@ package com.example.demo.model;
 
 import java.time.LocalDateTime;
 
-import org.springframework.data.annotation.Id;
+import org.springframework.boot.autoconfigure.web.WebProperties.Resources.Chain.Strategy;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 public class EmployeeProfile {
     @Id
-   private long id;
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private String employeedId;
-   private String fullName;
-@Email
-@Column(unique = true)
+      @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+   @Column(unique = true)
+    private String employeedId;
+    private String fullName;
+    @Email
+   @Column(unique = true)
     private String email;
     private String teamName;
     private String role;
-    private boolean active;
+    private Boolean active=true;
     private LocalDateTime createdAt;
+
+
+
+public EmployeeProfile()
+{
+
+}
+
+public EmployeeProfile( String employeedId, String fullName, String email, String teamName, String role,
+        Boolean active, LocalDateTime createdAt)
+         {
+  
+    this.employeedId = employeedId;
+    this.fullName = fullName;
+    this.email = email;
+    this.teamName = teamName;
+    this.role = role;
+    this.active = active;
+    this.createdAt = createdAt;
+}
+
+public long getId() {
+    return id;
+}
+
+public void setId(long id) {
+    this.id = id;
+}
+
+public String getEmployeedId() {
+    return employeedId;
+}
+
+public void setEmployeedId(String employeedId) {
+    this.employeedId = employeedId;
+}
+
+public String getFullName() {
+    return fullName;
+}
+
+public void setFullName(String fullName) {
+    this.fullName = fullName;
+}
+
+public String getEmail() {
+    return email;
+}
+
+public void setEmail(String email) {
+    this.email = email;
+}
+
+public String getTeamName() {
+    return teamName;
+}
+
+public void setTeamName(String teamName) {
+    this.teamName = teamName;
+}
+
+public String getRole() {
+    return role;
+}
+
+public void setRole(String role) {
+    this.role = role;
+}
+
+public Boolean getActive() {
+    return active;
+}
+
+public void setActive(Boolean active) {
+    this.active = active;
+}
+
+public LocalDateTime getCreatedAt() {
+    return createdAt;
+}
+
+public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+}
 }
 
