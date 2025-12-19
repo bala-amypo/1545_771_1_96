afe99bbeda2c6e3d2c4a2fd04162c66cb04ed91e
bala-amypo <bala@amypo.in>
2025-12-19 05:48:48 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/LeaveRequestController.java
@@ -0,0 +1,49 @@
+package com.example.demo.controller;
+
+import com.example.demo1.dto.LeaveRequestDto;
+import com.example.demo1.service.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/leaves")
+public class LeaveRequestController {
+
+    private final LeaveRequestService service;
+
+    public LeaveRequestController(LeaveRequestService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<LeaveRequestDto> create(@RequestBody LeaveRequestDto dto) {
+        return ResponseEntity.ok(service.create(dto));
+    }
+
+    @PutMapping("/{id}/approve")
+    public ResponseEntity<LeaveRequestDto> approve(@PathVariable Long id) {
+        return ResponseEntity.ok(service.approve(id));
+    }
+
+    @PutMapping("/{id}/reject")
+    public ResponseEntity<LeaveRequestDto> reject(@PathVariable Long id) {
+        return ResponseEntity.ok(service.reject(id));
+    }
+
+    @GetMapping("/employee/{employeeId}")
+    public ResponseEntity<List<LeaveRequestDto>> getByEmployee(@PathVariable Long employeeId) {
+        return ResponseEntity.ok(service.getByEmployee(employeeId));
+    }
+
+    @GetMapping("/team-overlap")
+    public ResponseEntity<List<LeaveRequestDto>> getOverlapping(
+            @RequestParam String teamName,
+            @RequestParam LocalDate start,
+            @RequestParam LocalDate end
+    ) {
+        return ResponseEntity.ok(service.getOverlappingForTeam(teamName, start, end));
+    }
+}
