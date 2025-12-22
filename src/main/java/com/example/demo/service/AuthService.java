e1e3623185f39f7b39526c2c14f6f663056883d9
bala-amypo <bala@amypo.in>
2025-12-22 03:56:33 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -0,0 +1,13 @@
+Service
+package com.example.demo.service;
+
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.model.UserAccount;
+
+public interface AuthService {
+
+    UserAccount register(UserAccount userAccount);
+
+    AuthResponse authenticate(AuthRequest request);
+}
